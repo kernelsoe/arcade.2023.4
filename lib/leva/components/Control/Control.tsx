28199be388a1192023297711bfe6a9941fe95598
Kernel Soe <kernelsoe@gmail.com>
2023-02-21 22:05:31 +0900

    fix eslint errors

--- lib/leva/components/Control/Control.tsx
+++ lib/leva/components/Control/Control.tsx
@@ -1,36 +1,39 @@
-import React from 'react'
-import { ControlInput } from './ControlInput'
-import { log, LevaErrors } from '../../utils/log'
-import { Plugins } from '../../plugin'
-import { Button } from '../Button'
-import { ButtonGroup } from '../ButtonGroup'
-import { Monitor } from '../Monitor'
-import { useInput } from '../../hooks'
-import { SpecialInputs } from '../../types'
+import React from "react";
+import { ControlInput } from "./ControlInput";
+import { log, LevaErrors } from "../../utils/log";
+import { Plugins } from "../../plugin";
+import { Button } from "../Button";
+import { ButtonGroup } from "../ButtonGroup";
+import { Monitor } from "../Monitor";
+import { useInput } from "../../hooks";
+import { SpecialInputs } from "../../types";
 
-type ControlProps = { path: string }
+type ControlProps = { path: string };
 
 const specialComponents = {
   [SpecialInputs.BUTTON]: Button,
   [SpecialInputs.BUTTON_GROUP]: ButtonGroup,
   [SpecialInputs.MONITOR]: Monitor,
-}
+};
 
 export const Control = React.memo(({ path }: ControlProps) => {
-  const [input, { set, setSettings, disable, storeId, emitOnEditStart, emitOnEditEnd }] = useInput(path)
-  if (!input) return null
+  const [
+    input,
+    { set, setSettings, disable, storeId, emitOnEditStart, emitOnEditEnd },
+  ] = useInput(path);
+  if (!input) return null;
 
-  const { type, label, key, ...inputProps } = input
+  const { type, label, key, ...inputProps } = input;
 
   if (type in SpecialInputs) {
     // @ts-expect-error
-    const SpecialInputForType = specialComponents[type]
-    return <SpecialInputForType label={label} path={path} {...inputProps} />
+    const SpecialInputForType = specialComponents[type];
+    return <SpecialInputForType label={label} path={path} {...inputProps} />;
   }
 
   if (!(type in Plugins)) {
-    log(LevaErrors.UNSUPPORTED_INPUT, type, path)
-    return null
+    log(LevaErrors.UNSUPPORTED_INPUT, type, path);
+    return null;
   }
 
   return (
@@ -49,5 +52,7 @@ export const Control = React.memo(({ path }: ControlProps) => {
       emitOnEditEnd={emitOnEditEnd}
       {...inputProps}
     />
-  )
-})
+  );
+});
+
+Control.displayName = "Control";
--- lib/leva/components/Leva/LevaRoot.tsx
+++ lib/leva/components/Leva/LevaRoot.tsx
@@ -1,40 +1,44 @@
-import React, { useMemo, useState } from 'react'
-import { buildTree } from './tree'
-import { TreeWrapper } from '../Folder'
+import React, { useMemo, useState } from "react";
+import { buildTree } from "./tree";
+import { TreeWrapper } from "../Folder";
 
-import { useDeepMemo, useTransform, useVisiblePaths } from '../../hooks'
+import { useDeepMemo, useTransform, useVisiblePaths } from "../../hooks";
 
-import { StyledRoot } from './StyledRoot'
-import { mergeTheme, LevaCustomTheme, globalStyles } from '../../styles'
-import { ThemeContext, StoreContext, PanelSettingsContext } from '../../context'
-import { TitleWithFilter } from './Filter'
-import { StoreType } from '../../types'
+import { StyledRoot } from "./StyledRoot";
+import { mergeTheme, LevaCustomTheme, globalStyles } from "../../styles";
+import {
+  ThemeContext,
+  StoreContext,
+  PanelSettingsContext,
+} from "../../context";
+import { TitleWithFilter } from "./Filter";
+import { StoreType } from "../../types";
 
 export type LevaRootProps = {
   /**
    * Theme with Stitches tokens
    */
-  theme?: LevaCustomTheme
+  theme?: LevaCustomTheme;
   /**
    * The store to be used by the panel
    */
-  store?: StoreType | null
+  store?: StoreType | null;
   /**
    * If true, won't display the panel
    */
-  hidden?: boolean
+  hidden?: boolean;
   /**
    * If true, will preset the panel even if no paths are defined
    */
-  neverHide?: boolean
+  neverHide?: boolean;
   /**
    * If true, the panel will fill its parent
    */
-  fill?: boolean
+  fill?: boolean;
   /**
    * If true, the panel will have no border radius nor shadow
    */
-  flat?: boolean
+  flat?: boolean;
   /**
    * If true, the panel will start collapsed.
    * If set to an object, the collapsed state is controlled via the property.
@@ -42,13 +46,13 @@ export type LevaRootProps = {
   collapsed?:
     | boolean
     | {
-        collapsed: boolean
-        onChange: (collapsed: boolean) => void
-      }
+        collapsed: boolean;
+        onChange: (collapsed: boolean) => void;
+      };
   /**
    * If true, input labels will stand above the controls
    */
-  oneLineLabels?: boolean
+  oneLineLabels?: boolean;
   /**
    * If true, the title bar including filters and drag zone will be shown. If set to false, the title bar including filters will be hidden.
    * In case it is set to an object the title bar will be shown and the additional sub-options might be applied.
@@ -59,58 +63,65 @@ export type LevaRootProps = {
         /**
          * Overwrites the default title content (6 dots if drag is enabled) if set to a non undefined value.
          */
-        title?: React.ReactNode
+        title?: React.ReactNode;
         /**
          * Toggle whether the leva panel can be dragged around via the title bar.
          */
-        drag?: boolean
+        drag?: boolean;
         /**
          * Toggle whether filtering should be enabled or disabled.
          */
-        filter?: boolean
+        filter?: boolean;
         /**
          * The position(x and y coordinates) of the leva panel.
          */
-        position?: { x?: number; y?: number }
+        position?: { x?: number; y?: number };
         /**
          * The callback is called when the leva panel is dragged.
          */
-        onDrag?: (position: { x?: number; y?: number }) => void
+        onDrag?: (position: { x?: number; y?: number }) => void;
         /**
          * The callback is called when the leva panel starts to be dragged.
          */
-        onDragStart?: (position: { x?: number; y?: number }) => void
+        onDragStart?: (position: { x?: number; y?: number }) => void;
         /**
          * The callback is called when the leva panel stops being dragged.
          */
-        onDragEnd?: (position: { x?: number; y?: number }) => void
-      }
+        onDragEnd?: (position: { x?: number; y?: number }) => void;
+      };
   /**
    * If true, the copy button will be hidden
    */
-  hideCopyButton?: boolean
-}
+  hideCopyButton?: boolean;
+};
 
-export function LevaRoot({ store, hidden = false, theme, collapsed = false, ...props }: LevaRootProps) {
-  const themeContext = useDeepMemo(() => mergeTheme(theme), [theme])
+export function LevaRoot({
+  store,
+  hidden = false,
+  theme,
+  collapsed = false,
+  ...props
+}: LevaRootProps) {
+  const themeContext = useDeepMemo(() => mergeTheme(theme), [theme]);
   // collapsible
-  const [toggled, setToggle] = useState(!collapsed)
+  const [toggled, setToggle] = useState(!collapsed);
 
-  const computedToggled = typeof collapsed === 'object' ? !collapsed.collapsed : toggled
+  const computedToggled =
+    typeof collapsed === "object" ? !collapsed.collapsed : toggled;
   const computedSetToggle = useMemo(() => {
-    if (typeof collapsed === 'object') {
+    if (typeof collapsed === "object") {
       return (value: React.SetStateAction<boolean>) => {
-        if (typeof value === 'function') {
-          collapsed.onChange(!value(!collapsed.collapsed))
+        if (typeof value === "function") {
+          collapsed.onChange(!value(!collapsed.collapsed));
         } else {
-          collapsed.onChange(!value)
+          collapsed.onChange(!value);
         }
-      }
+      };
     }
-    return setToggle
-  }, [collapsed])
+    return setToggle;
+  }, [collapsed]);
 
-  if (!store || hidden) return null
+  if (!store || hidden) return null;
 
   return (
     <ThemeContext.Provider value={themeContext}>
@@ -122,15 +133,15 @@ export function LevaRoot({ store, hidden = false, theme, collapsed = false, ...p
         rootClass={themeContext.className}
       />
     </ThemeContext.Provider>
-  )
+  );
 }
 
-type LevaCoreProps = Omit<LevaRootProps, 'theme' | 'hidden' | 'collapsed'> & {
-  store: StoreType
-  rootClass: string
-  toggled: boolean
-  setToggle: React.Dispatch<React.SetStateAction<boolean>>
-}
+type LevaCoreProps = Omit<LevaRootProps, "theme" | "hidden" | "collapsed"> & {
+  store: StoreType;
+  rootClass: string;
+  toggled: boolean;
+  setToggle: React.Dispatch<React.SetStateAction<boolean>>;
+};
 
 const LevaCore = React.memo(
   ({
@@ -153,28 +164,38 @@ const LevaCore = React.memo(
     toggled,
     setToggle,
   }: LevaCoreProps) => {
-    const paths = useVisiblePaths(store)
-    const [filter, setFilter] = useState('')
-    const tree = useMemo(() => buildTree(paths, filter), [paths, filter])
+    const paths = useVisiblePaths(store);
+    const [filter, setFilter] = useState("");
+    const tree = useMemo(() => buildTree(paths, filter), [paths, filter]);
 
     // drag
-    const [rootRef, set] = useTransform<HTMLDivElement>()
+    const [rootRef, set] = useTransform<HTMLDivElement>();
 
     // this generally happens on first render because the store is initialized in useEffect.
-    const shouldShow = neverHide || paths.length > 0
-    const title = typeof titleBar === 'object' ? titleBar.title || undefined : undefined
-    const drag = typeof titleBar === 'object' ? titleBar.drag ?? true : true
-    const filterEnabled = typeof titleBar === 'object' ? titleBar.filter ?? true : true
-    const position = typeof titleBar === 'object' ? titleBar.position || undefined : undefined
-    const onDrag = typeof titleBar === 'object' ? titleBar.onDrag || undefined : undefined
-    const onDragStart = typeof titleBar === 'object' ? titleBar.onDragStart || undefined : undefined
-    const onDragEnd = typeof titleBar === 'object' ? titleBar.onDragEnd || undefined : undefined
+    const shouldShow = neverHide || paths.length > 0;
+    const title =
+      typeof titleBar === "object" ? titleBar.title || undefined : undefined;
+    const drag = typeof titleBar === "object" ? titleBar.drag ?? true : true;
+    const filterEnabled =
+      typeof titleBar === "object" ? titleBar.filter ?? true : true;
+    const position =
+      typeof titleBar === "object" ? titleBar.position || undefined : undefined;
+    const onDrag =
+      typeof titleBar === "object" ? titleBar.onDrag || undefined : undefined;
+    const onDragStart =
+      typeof titleBar === "object"
+        ? titleBar.onDragStart || undefined
+        : undefined;
+    const onDragEnd =
+      typeof titleBar === "object"
+        ? titleBar.onDragEnd || undefined
+        : undefined;
 
     React.useEffect(() => {
-      set({ x: position?.x, y: position?.y })
-    }, [position, set])
+      set({ x: position?.x, y: position?.y });
+    }, [position, set]);
 
-    globalStyles()
+    globalStyles();
 
     return (
       <PanelSettingsContext.Provider value={{ hideCopyButton }}>
@@ -185,12 +206,13 @@ const LevaCore = React.memo(
           flat={flat}
           oneLineLabels={oneLineLabels}
           hideTitleBar={!titleBar}
-          style={{ display: shouldShow ? 'block' : 'none' }}>
+          style={{ display: shouldShow ? "block" : "none" }}
+        >
           {titleBar && (
             <TitleWithFilter
               onDrag={(point) => {
-                set(point)
-                onDrag?.(point)
+                set(point);
+                onDrag?.(point);
               }}
               onDragStart={(point) => onDragStart?.(point)}
               onDragEnd={(point) => onDragEnd?.(point)}
@@ -205,11 +227,19 @@ const LevaCore = React.memo(
           )}
           {shouldShow && (
             <StoreContext.Provider value={store}>
-              <TreeWrapper isRoot fill={fill} flat={flat} tree={tree} toggled={toggled} />
+              <TreeWrapper
+                isRoot
+                fill={fill}
+                flat={flat}
+                tree={tree}
+                toggled={toggled}
+              />
             </StoreContext.Provider>
           )}
         </StyledRoot>
       </PanelSettingsContext.Provider>
-    )
-  }
-)
+    );
+  },
+);
+
+LevaCore.displayName = "LevaCore";
